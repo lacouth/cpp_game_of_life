b26249d22abd1ae20bec758ad556733f18f58bb3
lacouth <lacouth@gmail.com>
2021-09-25 17:44:44 -0300

    organizes the functions declarations

--- main.cpp
+++ main.cpp
@@ -17,11 +17,61 @@ const std::string DEAD_SYMBOL = " _ ";
 const int MIN_NEIGHBORS = 2;
 const int MAX_NEIGHBORS = 3;
 
-const std::vector<std::vector<int>> neighbors{
-    {-1, 0}, {0, -1}, {1, 0}, {0, 1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+const std::vector<std::vector<int>> neighbors{{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
 
-Board board_factory(size_t n_rows, size_t n_cols,
-                    Cell initial_value = Cell::dead) {
+Board board_factory(size_t n_rows, size_t n_cols, Cell initial_value = Cell::dead);
+void generates_board_initial_state(Board &board, size_t number_of_cells);
+void print_board(const Board &board);
+void update_board(Board &board);
+bool is_everybody_dead(const Board &board);
+
+int main(int argc, char **argv) {
+
+  size_t board_size = 50;
+  uint number_initial_living_cells = 20;
+  size_t generations = 0;
+  size_t max_generations = 100;
+
+  while (true) {
+    switch (getopt(argc, argv, "s:n:m:")) {
+    case 's':
+      board_size = std::stoi(optarg);
+      continue;
+    case 'n':
+      number_initial_living_cells = std::stoi(optarg);
+      continue;
+    case 'm':
+      max_generations = std::stoi(optarg);
+      continue;
+    default:
+      break;
+    }
+    break;
+  }
+
+  Board board = board_factory(board_size, board_size);
+
+  generates_board_initial_state(board, number_initial_living_cells);
+  while (!is_everybody_dead(board) && generations < max_generations) {
+    print_board(board);
+    update_board(board);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    generations++;
+  }
+  print_board(board);
+  if (generations < max_generations)
+    std::cout << "GAME OVER - No Cells Alive\n";
+  else
+    std::cout << generations << " generations\n";
+}
+
+/*
+
+    Utility Functions
+
+*/
+
+Board board_factory(size_t n_rows, size_t n_cols, Cell initial_value) {
   return Board(n_rows, Row(n_cols, initial_value));
 }
 
@@ -57,7 +107,7 @@ std::pair<int, int> check_neighbor_position(const std::vector<int> &coord,
   for (int i = 0; i < 2; ++i) {
     new_coords[i] = positions[i] + coord[i];
     if (new_coords[i] < 0 || new_coords[i] >= board_size) {
-      new_coords[i] = new_coords[i] < 0 ? 0 : board_size - 1;
+      new_coords[i] = new_coords[i] < 0 ? board_size - 1 : 0;
     }
   }
 
@@ -100,44 +150,4 @@ bool is_everybody_dead(const Board &board) {
     });
   });
   return flag;
-}
-
-int main(int argc, char **argv) {
-
-  size_t board_size = 50;
-  uint number_initial_living_cells = 5;
-  size_t generations = 0;
-  size_t max_generations = 100;
-
-  while (true) {
-    switch (getopt(argc, argv, "s:n:m:")) {
-    case 's':
-      board_size = std::stoi(optarg);
-      continue;
-    case 'n':
-      number_initial_living_cells = std::stoi(optarg);
-      continue;
-    case 'm':
-      max_generations = std::stoi(optarg);
-      continue;
-    default:
-      break;
-    }
-    break;
-  }
-
-  Board board = board_factory(board_size, board_size);
-
-  generates_board_initial_state(board, number_initial_living_cells);
-  while (!is_everybody_dead(board) && generations < max_generations) {
-    print_board(board);
-    update_board(board);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    generations++;
-  }
-  print_board(board);
-  if (generations < max_generations)
-    std::cout << "GAME OVER - No Cells Alive\n";
-  else
-    std::cout << generations << " generations\n";
 }
