b80aeb150ba8ebd4744e5852468da8465f29f992
lacouth <lacouth@gmail.com>
2021-09-25 12:07:13 -0300

    adds print_board function

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <string>
 #include <random>
+#include <string>
+#include <vector>
 
-typedef std::vector<std::vector<uint>> Board;
+typedef std::vector<uint> Row;
+typedef std::vector<Row> Board;
+const std::string ALIVE = " o ";
+const std::string DEAD = " _ ";
 
-void generates_board_initial_state(Board &board, size_t number_of_cells){
+void generates_board_initial_state(Board &board, size_t number_of_cells) {
 
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> random_number(0,board.size() - 1);
-    
-    for(int i=0;i<number_of_cells;++i){
-        board[random_number(rng)][random_number(rng)] = 1;
-    }
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> random_number(
+      0, board.size() - 1);
 
+  for (int i = 0; i < number_of_cells; ++i) {
+    board[random_number(rng)][random_number(rng)] = 1;
+  }
 }
 
-int main(){
+void print_board(const Board &board) {
 
-    size_t board_width = 25;
-    size_t number_initial_live_cells = 10;
-    
-    Board board(board_width, std::vector<uint>(board_width,0));
+  std::for_each(board.begin(), board.end(), [](const Row &row) {
+    std::for_each(row.begin(), row.end(),
+                  [](const uint &cell) { std::cout << (cell ? ALIVE : DEAD); });
+    std::cout << std::endl;
+  });
+}
 
-    generates_board_initial_state(board,number_initial_live_cells);
-    
+int main() {
+
+  size_t board_width = 10;
+  size_t number_initial_live_cells = 20;
+
+  Board board(board_width, std::vector<uint>(board_width, 0));
+
+  generates_board_initial_state(board, number_initial_live_cells);
+  print_board(board);
 }
